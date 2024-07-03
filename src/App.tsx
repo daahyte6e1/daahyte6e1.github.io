228dd9fe8176a3ab79fffbf21e3814193d363426
s.shakhbazyan <s.shakhbazyan@kt-team.de>
2024-07-03 09:04:04 +0400

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import twaLogo from './assets/tapps.png'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import WebApp from '@twa-dev/sdk'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count] = useState(0)
 
   return (
     <>
